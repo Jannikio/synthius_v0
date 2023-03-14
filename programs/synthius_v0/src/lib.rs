fab16a2e26afcc52f242d7a12f8c9a72d4f7ab17
Jannik <theofficialjannik@gmail.com>
2023-03-14 17:35:10 +0100

    added code

--- programs/synthius_v0/src/lib.rs
+++ programs/synthius_v0/src/lib.rs
@@ -1,4 +1,14 @@
+use std::mem::size_of;
 use anchor_lang::prelude::*;
+use solana_program::account_info::AccountInfo;
+use anchor_spl::{token, associated_token};
+
+pub mod pyth;
+use pyth::PriceFeed;
+use pyth::AdminConfig;
+
+mod error;
+use error::ErrorCode;
 
 declare_id!("3o5VrciviJWYnB39NNfmsWTNqSa4aooXKUf5AzZMdWXu");
 
@@ -6,10 +16,371 @@ declare_id!("3o5VrciviJWYnB39NNfmsWTNqSa4aooXKUf5AzZMdWXu");
 pub mod synthius_v0 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, config: AdminConfig) -> Result<()> {
+        ctx.accounts.config.set_inner(config);
+        msg!("Initialize accounts");
+        Ok(())
+    }
+
+    pub fn dummy_token(ctx: Context<DummyToken>, amount: u64) -> Result<()> {
+        let cpi_accounts = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::MintTo {
+                mint: ctx.accounts.collateral_token_mint.to_account_info(),
+                to: ctx.accounts.collateral_token_account.to_account_info(),
+                authority: ctx.accounts.payer.to_account_info(),
+            },);
+        token::mint_to(cpi_accounts, amount)?;
+        msg!("Dummy token");
+        Ok(())
+    }
+
+    pub fn buy_long(ctx: Context<BuyLong>, amount: u64) -> Result<()> {
+        ctx.accounts.vault.amount += amount;
+        ctx.accounts.vault.position = Position::Long;
+        let vault = &mut (ctx.accounts.vault);
+        let price_feed = &ctx.accounts.pyth_loan_account;
+        let current_timestamp = Clock::get()?.unix_timestamp;
+        let stock_price = price_feed
+            .get_price_no_older_than(current_timestamp, 60)
+            .ok_or(error!(ErrorCode::PythOffline))?;
+        vault.price_entered = stock_price.price;
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.collateral_token_account.to_account_info(),
+                to: ctx.accounts.vault_wallet.to_account_info(),
+                authority: ctx.accounts.payer.to_account_info(),
+            },
+        );
+        token::transfer(cpi_context, amount)?;
+        token::mint_to(CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::MintTo {
+                mint: ctx.accounts.long_token_mint.to_account_info(),
+                to: ctx.accounts.long_token_account.to_account_info(),
+                authority: ctx.accounts.mint_authority.to_account_info(),
+            },
+        ), 1)?;
+        msg!("Buy long");
+        Ok(())
+    }
+
+    pub  fn sell_long(ctx: Context<SellLong>, bump: u8, signer: Pubkey) -> Result<()> {
+        let signer_pubkey = signer.key();
+        let signer = signer_pubkey.as_ref();
+        let seeds =  &[&[b"vault", signer , anchor_lang::__private::bytemuck::bytes_of(&bump)][..]];
+        let collateral = ctx.accounts.vault.amount;
+        let price_entered = ctx.accounts.vault.price_entered;
+        let price_feed = &ctx.accounts.pyth_loan_account;
+        let current_timestamp = Clock::get()?.unix_timestamp;
+        let stock_price = price_feed
+            .get_price_no_older_than(current_timestamp, 60)
+            .ok_or(error!(ErrorCode::PythOffline))?;
+        let remaining_collateral: i64;
+        if ctx.accounts.vault.position != Position::Long {
+            return Err(error!(ErrorCode::InvalidArgument));
+        }
+        token::burn(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                token::Burn {
+                    mint: ctx.accounts.long_token_mint.to_account_info(),
+                    from: ctx.accounts.long_token_account.to_account_info(),
+                    authority: ctx.accounts.mint_authority.to_account_info(),
+                }
+            ),1)?;
+        if stock_price.price > price_entered {
+            let profit = stock_price.price - price_entered;
+            remaining_collateral = (collateral as i64) + profit;
+            msg!("You made a profit of {}!", remaining_collateral);
+        } else {
+            let loss = price_entered - stock_price.price;
+            remaining_collateral = (collateral as i64) - loss;
+            msg!("You made a loss of {}!", remaining_collateral);
+        }
+
+        let cpi_context = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.vault_wallet.to_account_info(),
+                to: ctx.accounts.collateral_token_account.to_account_info(),
+                authority: ctx.accounts.vault.to_account_info(),
+            },
+            seeds,
+        );
+        token::transfer(cpi_context, remaining_collateral as u64)?;
+        Ok(())
+
+    }
+
+    pub fn buy_short(ctx: Context<BuyShort>, amount: u64) -> Result<()> {
+        ctx.accounts.vault.amount += amount;
+        ctx.accounts.vault.position = Position::Short;
+        let vault = &mut (ctx.accounts.vault);
+        let price_feed = &ctx.accounts.pyth_loan_account;
+        let current_timestamp = Clock::get()?.unix_timestamp;
+        let stock_price = price_feed
+            .get_price_no_older_than(current_timestamp, 60)
+            .ok_or(error!(ErrorCode::PythOffline))?;
+        vault.price_entered = stock_price.price;
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.collateral_token_account.to_account_info(),
+                to: ctx.accounts.vault_wallet.to_account_info(),
+                authority: ctx.accounts.payer.to_account_info(),
+            },
+        );
+        token::transfer(cpi_context, amount)?;
+        token::mint_to(CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::MintTo {
+                mint: ctx.accounts.short_token_mint.to_account_info(),
+                to: ctx.accounts.short_token_account.to_account_info(),
+                authority: ctx.accounts.mint_authority.to_account_info(),
+            },
+        ), 1)?;
+        msg!("Buy short");
+        Ok(())
+    }
+
+    pub fn sell_short(ctx: Context<SellShort>, bump: u8, signer: Pubkey) -> Result<()> {
+        let signer_pubkey = signer.key();
+        let signer = signer_pubkey.as_ref();
+        let seeds =  &[&[b"vault", signer , anchor_lang::__private::bytemuck::bytes_of(&bump)][..]];
+        let collateral = ctx.accounts.vault.amount;
+        let price_entered = ctx.accounts.vault.price_entered;
+        let price_feed = &ctx.accounts.pyth_loan_account;
+        let current_timestamp = Clock::get()?.unix_timestamp;
+        let stock_price = price_feed
+            .get_price_no_older_than(current_timestamp, 60)
+            .ok_or(error!(ErrorCode::PythOffline))?;
+        let remaining_collateral: i64;
+        if ctx.accounts.vault.position != Position::Short {
+            return Err(error!(ErrorCode::InvalidArgument));
+        }
+        token::burn(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                token::Burn {
+                    mint: ctx.accounts.short_token_mint.to_account_info(),
+                    from: ctx.accounts.short_token_account.to_account_info(),
+                    authority: ctx.accounts.mint_authority.to_account_info(),
+                }
+            ),1)?;
+        if stock_price.price < price_entered {
+            let profit = price_entered - stock_price.price;
+            remaining_collateral = (collateral as i64) + profit;
+            msg!("You made a profit of {}!", remaining_collateral);
+        } else {
+            let loss = stock_price.price - price_entered;
+            remaining_collateral = (collateral as i64) - loss;
+            msg!("You made a loss of {}!", remaining_collateral);
+        }
+        let cpi_context = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.vault_wallet.to_account_info(),
+                to: ctx.accounts.collateral_token_account.to_account_info(),
+                authority: ctx.accounts.vault.to_account_info(),
+            },
+            seeds,
+        );
+        token::transfer(cpi_context, remaining_collateral as u64)?;
+        Ok(())
+    }
+
+    pub fn add_liquidity(ctx: Context<AddLiquidity>, amount: u64) -> Result<()> {
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.collateral_token_account.to_account_info(),
+                to: ctx.accounts.vault_wallet.to_account_info(),
+                authority: ctx.accounts.payer.to_account_info(),
+            },
+        );
+        token::transfer(cpi_context, amount)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(address = *program_id @ ErrorCode::Unauthorized)]
+    pub program: Signer<'info>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(init_if_needed, payer = payer, space = 8 + size_of::<AdminConfig>())]
+    pub config: Account<'info, AdminConfig>,
+    pub system_program: Program<'info, System>,
+    #[account(init_if_needed, payer = payer, space = 8 + size_of::<Vault>(), seeds = [b"vault".as_ref(), payer.key.as_ref()], bump)]
+    pub vault: Account<'info, Vault>,
+}
+
+#[derive(Accounts)]
+pub struct DummyToken<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+    #[account(init, payer = payer, mint::decimals = 9, mint::authority = mint_authority)]
+    pub collateral_token_mint: Account<'info, token::Mint>,
+    pub mint_authority: SystemAccount<'info>,
+    #[account(init, payer = payer, associated_token::mint = collateral_token_mint, associated_token::authority = payer)]
+    pub collateral_token_account: Account<'info, token::TokenAccount>,
+}
+
+#[derive(Accounts)]
+pub struct BuyLong<'info> {
+    pub config: Account<'info, AdminConfig>,
+    #[account(address = config.loan_price_feed_id @ ErrorCode::InvalidArgument)]
+    pub pyth_loan_account: Account<'info, PriceFeed>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+    #[account(init, payer = payer, mint::decimals = 9, mint::authority = mint_authority)]
+    pub long_token_mint: Account<'info, token::Mint>,
+    pub mint_authority: SystemAccount<'info>,
+    #[account(init_if_needed, payer = payer, associated_token::mint = long_token_mint, associated_token::authority = payer)]
+    pub long_token_account: Account<'info, token::TokenAccount>,
+    #[account(mut, seeds = [b"vault".as_ref(), payer.key.as_ref()], bump)]
+    pub vault: Account<'info, Vault>,
+    #[account(mut)]
+    pub collateral_token_mint: Account<'info, token::Mint>,
+    #[account(mut, associated_token::mint = collateral_token_mint, associated_token::authority = payer)]
+    pub collateral_token_account: Account<'info, token::TokenAccount>,
+    #[account(init_if_needed,
+        payer = payer,
+        token::mint = collateral_token_mint,
+        token::authority = vault,
+        seeds = [b"vaultWallet".as_ref(), payer.key.as_ref()],bump
+    )]
+    pub vault_wallet: Account<'info, token::TokenAccount>,
+}
+
+#[derive(Accounts)]
+pub struct SellLong<'info> {
+    pub config: Account<'info, AdminConfig>,
+    #[account(address = config.loan_price_feed_id @ ErrorCode::InvalidArgument)]
+    pub pyth_loan_account: Account<'info, PriceFeed>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+    #[account(mut, seeds = [b"vault".as_ref(), payer.key.as_ref()], bump)]
+    pub vault: Account<'info, Vault>,
+    #[account(mut)]
+    pub long_token_mint: Account<'info, token::Mint>,
+    pub mint_authority: SystemAccount<'info>,
+    #[account(mut, associated_token::mint = long_token_mint, associated_token::authority = payer)]
+    pub long_token_account: Account<'info, token::TokenAccount>,
+    #[account(mut)]
+    pub collateral_token_mint: Account<'info, token::Mint>,
+    #[account(mut, associated_token::mint = collateral_token_mint, associated_token::authority = payer)]
+    pub collateral_token_account: Account<'info, token::TokenAccount>,
+    #[account(mut,
+        token::mint = collateral_token_mint,
+        token::authority = vault,
+        seeds = [b"vaultWallet".as_ref(), payer.key.as_ref()],bump
+    )]
+    pub vault_wallet: Account<'info, token::TokenAccount>,
+}
+
+#[derive(Accounts)]
+pub struct BuyShort<'info> {
+    pub config: Account<'info, AdminConfig>,
+    #[account(address = config.loan_price_feed_id @ ErrorCode::InvalidArgument)]
+    pub pyth_loan_account: Account<'info, PriceFeed>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+    #[account(init, payer = payer, mint::decimals = 9, mint::authority = mint_authority)]
+    pub short_token_mint: Account<'info, token::Mint>,
+    pub mint_authority: SystemAccount<'info>,
+    #[account(init_if_needed, payer = payer, associated_token::mint = short_token_mint, associated_token::authority = payer)]
+    pub short_token_account: Account<'info, token::TokenAccount>,
+    #[account(mut, seeds = [b"vault".as_ref(), payer.key.as_ref()], bump)]
+    pub vault: Account<'info, Vault>,
+    #[account(mut)]
+    pub collateral_token_mint: Account<'info, token::Mint>,
+    #[account(mut, associated_token::mint = collateral_token_mint, associated_token::authority = payer)]
+    pub collateral_token_account: Account<'info, token::TokenAccount>,
+    #[account(init_if_needed,
+        payer = payer,
+        token::mint = collateral_token_mint,
+        token::authority = vault,
+        seeds = [b"vault".as_ref(), payer.key.as_ref()],bump
+    )]
+    pub vault_wallet: Account<'info, token::TokenAccount>,
+}
+
+#[derive(Accounts)]
+pub struct SellShort<'info> {
+    pub config: Account<'info, AdminConfig>,
+    #[account(address = config.loan_price_feed_id @ ErrorCode::InvalidArgument)]
+    pub pyth_loan_account: Account<'info, PriceFeed>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+    #[account(mut, seeds = [b"vault".as_ref(), payer.key.as_ref()], bump)]
+    pub vault: Account<'info, Vault>,
+    #[account(mut)]
+    pub short_token_mint: Account<'info, token::Mint>,
+    pub mint_authority: SystemAccount<'info>,
+    #[account(mut, associated_token::mint = short_token_mint, associated_token::authority = payer)]
+    pub short_token_account: Account<'info, token::TokenAccount>,
+    #[account(mut)]
+    pub collateral_token_mint: Account<'info, token::Mint>,
+    #[account(mut, associated_token::mint = collateral_token_mint, associated_token::authority = payer)]
+    pub collateral_token_account: Account<'info, token::TokenAccount>,
+    #[account(mut,
+        token::mint = collateral_token_mint,
+        token::authority = vault,
+        seeds = [b"vault".as_ref(), payer.key.as_ref()],bump
+    )]
+    pub vault_wallet: Account<'info, token::TokenAccount>,
+}
+
+#[derive(Accounts)]
+pub struct AddLiquidity<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+    #[account(mut)]
+    pub collateral_token_mint: Account<'info, token::Mint>,
+    #[account(mut, associated_token::mint = collateral_token_mint, associated_token::authority = payer)]
+    pub collateral_token_account: Account<'info, token::TokenAccount>,
+    #[account(mut, seeds = [b"vault".as_ref(), payer.key.as_ref()], bump)]
+    pub vault: Account<'info, Vault>,
+    #[account(mut,
+        token::mint = collateral_token_mint,
+        token::authority = vault,
+        seeds = [b"vault".as_ref(), payer.key.as_ref()],bump
+    )]
+    pub vault_wallet: Account<'info, token::TokenAccount>
+}
+
+#[account]
+pub struct Vault {
+    pub amount: u64,
+    pub price_entered: i64,
+    pub position: Position,
+    pub collateral_locked: u64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum Position {
+    Long,
+    Short,
+}
--- tests/synthius_v0.ts
+++ tests/synthius_v0.ts
@@ -1,16 +1,199 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SynthiusV0 } from "../target/types/synthius_v0";
+import fs from "fs";
+import assert from "assert";
+import { PublicKey } from "@solana/web3.js";
+import { ASSOCIATED_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
 
 describe("synthius_v0", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SynthiusV0 as Program<SynthiusV0>;
+  const programId = program.programId;
+  let example_price = "G7dySNGaxZ8y2E89aX1K6rFeBt2ZnYBqXuCu1k2Y9MEe";
+
+  const longMintKeypair = anchor.web3.Keypair.generate();
+  const shortMintKeypair = anchor.web3.Keypair.generate();
+  const collateralMintKeypair = anchor.web3.Keypair.generate();
+
+  const depositedAmount = new anchor.BN(1);
+  const dummyTokenAmount = new anchor.BN(5);
+
+  const config = anchor.web3.Keypair.generate();
+  const payer = provider.wallet as anchor.Wallet;
+  const [vaultKey, vaultBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from(anchor.utils.bytes.utf8.encode("vault")), payer.publicKey.toBuffer()], programId
+  );
+  const vaultWalletKey = PublicKey.findProgramAddressSync(
+    [Buffer.from(anchor.utils.bytes.utf8.encode("vaultWallet")), payer.publicKey.toBuffer()], programId
+  ) [0];
+
+  var programKey;
+  try {
+      let data = fs.readFileSync(
+          '/Users/jannikspilker/Desktop/Grizzlython/v0/target/deploy/v0-keypair.json',
+      );
+      programKey = anchor.web3.Keypair.fromSecretKey(
+          new Uint8Array(JSON.parse(data))
+      );
+  } catch (error) {
+      throw new Error("Please make sure the program key is program_address.json.");
+  }
+
+  try {
+      assert(programId.equals(programKey.publicKey));
+  } catch (error) {
+      throw new Error("Please make sure you have the same program address in Anchor.toml and program_address.json");
+  }
+
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initialize({
+      loanPriceFeedId: new anchor.web3.PublicKey(example_price)
+    }).accounts({
+      program: programId,
+      payer: payer.publicKey,
+      config: config.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      vault: vaultKey, 
+    }).signers([config, programKey]).rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Mints dummy token", async () => {
+    const associatedTokenAddressCollateral = 
+          await anchor.utils.token.associatedAddress({mint: collateralMintKeypair.publicKey, owner: payer.publicKey});
+    let tx = await program.methods.dummyToken(dummyTokenAmount).
+            accounts({
+              payer: payer.publicKey,
+              systemProgram: anchor.web3.SystemProgram.programId,
+              tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+              associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+              collateralTokenMint: collateralMintKeypair.publicKey,
+              mintAuthority: payer.publicKey,
+              collateralTokenAccount: associatedTokenAddressCollateral
+            }).signers([payer.payer, collateralMintKeypair]).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Buys Long", async () => {
+    const associatedTokenAddressCollateral =
+          await anchor.utils.token.associatedAddress({mint: collateralMintKeypair.publicKey, owner: payer.publicKey});
+    const associatedTokenAddressLongToken = 
+          await anchor.utils.token.associatedAddress({mint: longMintKeypair.publicKey, owner: payer.publicKey});
+
+    let tx = await program.methods.buyLong(depositedAmount)
+            .accounts({
+              config: config.publicKey,
+              pythLoanAccount: new anchor.web3.PublicKey(example_price),
+              payer: payer.publicKey,
+              systemProgram: anchor.web3.SystemProgram.programId,
+              tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+              associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+              longTokenMint: longMintKeypair.publicKey,
+              mintAuthority: payer.publicKey,
+              longTokenAccount: associatedTokenAddressLongToken,
+              vault: vaultKey,
+              collateralTokenMint: collateralMintKeypair.publicKey,
+              collateralTokenAccount: associatedTokenAddressCollateral,
+              vaultWallet: vaultWalletKey
+            }).signers([payer.payer, longMintKeypair]).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Sells Long", async() => {
+    const associatedTokenAddressCollateral =
+          await anchor.utils.token.associatedAddress({mint: collateralMintKeypair.publicKey, owner: payer.publicKey});
+    const associatedTokenAddressLongToken = 
+          await anchor.utils.token.associatedAddress({mint: longMintKeypair.publicKey, owner: payer.publicKey});
+          
+    let tx = await program.methods.sellLong(vaultBump, payer.publicKey)
+              .accounts({
+                config: config.publicKey,
+                pythLoanAccount: new anchor.web3.PublicKey(example_price),
+                payer: payer.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+                tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+                associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+                longTokenMint: longMintKeypair.publicKey,
+                mintAuthority: payer.publicKey,
+                longTokenAccount: associatedTokenAddressLongToken,
+                collateralTokenMint: collateralMintKeypair.publicKey,
+                collateralTokenAccount: associatedTokenAddressCollateral,
+                vaultWallet: vaultWalletKey,
+                vault: vaultKey
+              }).signers([payer.payer]).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Buys short",async () => {
+    const associatedTokenAddressCollateral =
+          await anchor.utils.token.associatedAddress({mint: collateralMintKeypair.publicKey, owner: payer.publicKey});
+    const associatedTokenAddressShortToken = 
+          await anchor.utils.token.associatedAddress({mint: shortMintKeypair.publicKey, owner: payer.publicKey});
+    
+    let tx = await program.methods.buyShort(depositedAmount)
+            .accounts({
+              config: config.publicKey,
+              pythLoanAccount: new anchor.web3.PublicKey(example_price),
+              payer: payer.publicKey,
+              systemProgram: anchor.web3.SystemProgram.programId,
+              tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+              associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+              shortTokenMint: shortMintKeypair.publicKey,
+              mintAuthority: payer.publicKey,
+              shortTokenAccount: associatedTokenAddressShortToken,
+              vault: vaultKey,
+              collateralTokenMint: collateralMintKeypair.publicKey,
+              collateralTokenAccount: associatedTokenAddressCollateral,
+              vaultWallet: vaultWalletKey
+            }).signers([payer.payer, shortMintKeypair]).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Sells short",async () => {
+    const associatedTokenAddressCollateral =
+          await anchor.utils.token.associatedAddress({mint: collateralMintKeypair.publicKey, owner: payer.publicKey});
+    const associatedTokenAddressShortToken = 
+          await anchor.utils.token.associatedAddress({mint: shortMintKeypair.publicKey, owner: payer.publicKey});
+    let tx = await program.methods.sellShort(vaultBump, payer.publicKey)
+            .accounts({
+              config: config.publicKey,
+              pythLoanAccount: new anchor.web3.PublicKey(example_price),
+              payer: payer.publicKey,
+              systemProgram: anchor.web3.SystemProgram.programId,
+              tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+              associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+              vault: vaultKey,
+              shortTokenMint: shortMintKeypair.publicKey,
+              mintAuthority: payer.publicKey,
+              shortTokenAccount: associatedTokenAddressShortToken,
+              collateralTokenMint: collateralMintKeypair.publicKey,
+              collateralTokenAccount: associatedTokenAddressCollateral,
+              vaultWallet: vaultWalletKey,
+            }).signers([payer.payer]).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Adds liquidity",async () => {
+    const associatedTokenAddressCollateral =
+          await anchor.utils.token.associatedAddress({mint: collateralMintKeypair.publicKey, owner: payer.publicKey});
+
+    let tx = await program.methods.addLiquidity(depositedAmount)
+              .accounts({
+                payer: payer.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+                tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+                associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+                collateralTokenMint: collateralMintKeypair.publicKey,
+                collateralTokenAccount: associatedTokenAddressCollateral,
+                vault: vaultKey,
+                vaultWallet: vaultWalletKey
+              }).signers([payer.payer]).rpc();
+  })
+
 });
